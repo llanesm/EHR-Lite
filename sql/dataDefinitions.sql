118cd91342b7b914d2bb1029708a7e703f8fde9d
llanesm <llanesm@oregonstate.edu>
2020-11-13 15:10:38 -0700

    added more sample data, added ON DELETE instruction to the M:M relationships

--- sql/dataDefinitions.sql
+++ sql/dataDefinitions.sql
@@ -25,7 +25,7 @@ CREATE TABLE `providers` (
     `fname` varchar(255),
     `lname` varchar(255),
     `licenseType` varchar(255),
-    `licenseNumber` int(11),
+    `licenseNumber` int(11) UNIQUE,
     `specialty` varchar(255),
     `primaryCare` boolean NOT NULL DEFAULT 0,
     PRIMARY KEY (`providerID`)
@@ -75,34 +75,52 @@ CREATE TABLE `visits` (
 CREATE TABLE `providersClinics` (
     `providerID` int(11),
     `clinicID` int(11),
-    FOREIGN KEY (`providerID`) REFERENCES providers(`providerID`),
-    FOREIGN KEY (`clinicID`) REFERENCES clinics(`clinicID`)
+    FOREIGN KEY (`providerID`) REFERENCES providers(`providerID`) ON DELETE CASCADE,
+    FOREIGN KEY (`clinicID`) REFERENCES clinics(`clinicID`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE TABLE `patientsClinics` (
     `patientID` int(11),
     `clinicID` int(11),
-    FOREIGN KEY (`patientID`) REFERENCES patients(`medicalRecordNumber`),
-    FOREIGN KEY (`clinicID`) REFERENCES clinics(`clinicID`)
+    FOREIGN KEY (`patientID`) REFERENCES patients(`medicalRecordNumber`) ON DELETE CASCADE,
+    FOREIGN KEY (`clinicID`) REFERENCES clinics(`clinicID`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- Insertions to populate each table.
 
 INSERT INTO clinics (clinicName, specialty, providerCapacity, examRooms, primaryCare)
-VALUES ("Health Plaza", "Family Medicine", 5, 5, TRUE);
+VALUES ("Max Health Plaza", "Family Medicine", 5, 5, TRUE),
+("Max Orthopedics", "Orthopedic Surgery", 2, 2, FALSE);
 
 INSERT INTO providers (fname, lname, licenseType, licenseNumber, specialty, primaryCare)
-VALUES ("John", "Smith", "Medical Doctor", 1234, "Family Medicine", TRUE);
+VALUES ("John", "Smith", "Medical Doctor", 1234, "Family Medicine", TRUE),
+("Mary", "Jane", "Medical Doctor", 9482, "Family Medicine", TRUE),
+("Martha", "Mathers", "Nurse Practitioner", 2856, "Family Medicine", TRUE),
+("Gerald", "Bones", "Medical Doctor", 8546, "Orthopedic Surgery", FALSE),
+("Jerry", "Knee", "Physician Assistant", 1112, "Orthopedic Surgery", FALSE);
 
 INSERT INTO patients (fname, lname, birthdate, preferredPharmacy, primaryCarePhysician)
 VALUES ("Deborah", "Donahugh", "1972-06-12", "Walgreens",
-(SELECT providerID FROM providers WHERE fname="John" AND lname="Smith"));
+(SELECT providerID FROM providers WHERE fname="John" AND lname="Smith")),
+("Maddie", "Acosta", "1994-04-20", "RiteAid",
+(SELECT providerID FROM providers WHERE fname="Mary" AND lname="Jane")),
+("Ember", "Bobember", "2018-03-16", "Walmart",
+(SELECT providerID FROM providers WHERE fname="Martha" AND lname="Mathers")),
+("Bob", "Burgers", "1956-12-10", "Walgreens",
+(SELECT providerID FROM providers WHERE fname="John" AND lname="Smith")),
+("Rick", "Sanchez", "1950-01-01", "Shmorkazorp Medicinals",
+(SELECT providerID FROM providers WHERE fname="Martha" AND lname="Mathers"));
 
 INSERT INTO procedures (procedureCode, procedureName)
-VALUES (99214, "General Checkup");
+VALUES (99214, "General Checkup"),
+(29880, "Arthroscopy, knee, surgical; with meniscectomy"),
+(11730, "Avulsion of nail plate, partial or complete, simple, single");
 
 INSERT INTO diagnoses (diagnosisCode, diagnosisName)
-VALUES ("I10", "Essential Hypertension");
+VALUES ("I10", "Essential Hypertension"),
+("E11*", "Type 2 Diabetes Mellitus"),
+("L60.0", "Ingrown toenail"),
+("S83", "Torn meniscus");
 
 INSERT INTO visits (visitDate, chiefComplaint, diagnosisCode, procedureCode, patient, provider, clinicID, providerNotes)
 VALUES ("2020-03-20", "Wellness",
@@ -110,6 +128,80 @@ VALUES ("2020-03-20", "Wellness",
     (SELECT procedureCode FROM procedures WHERE procedureName="General Checkup"),
     (SELECT medicalRecordNumber FROM patients WHERE fname="Deborah" AND lname="Donahugh" AND birthdate="1972-06-12"),
     (SELECT providerID FROM providers WHERE fname="John" AND lname="Smith"),
-    (SELECT clinicID FROM clinics WHERE clinicName="Health Plaza"),
-    "Deborah got her yearly wellness, hypertension continues to be stable on current medication"
-);
+    (SELECT clinicID FROM clinics WHERE clinicName="Max Health Plaza"),
+    "Deborah got her yearly wellness, hypertension continues to be stable on current medication"),
+("2020-07-03", "Wellness",
+    NULL,
+    (SELECT procedureCode FROM procedures WHERE procedureName="General Checkup"),
+    (SELECT medicalRecordNumber FROM patients WHERE fname="Maddie" AND lname="Acosta" AND birthdate="1994-04-20"),
+    (SELECT providerID FROM providers WHERE fname="Mary" AND lname="Jane"),
+    (SELECT clinicID FROM clinics WHERE clinicName="Max Health Plaza"),
+    "Maddie got her yearly wellness, continues to be a picture of health"),
+("2020-02-18", "Wellness",
+    (SELECT diagnosisCode FROM diagnoses WHERE diagnosisName="Essential Hypertension"),
+    (SELECT procedureCode FROM procedures WHERE procedureName="General Checkup"),
+    (SELECT medicalRecordNumber FROM patients WHERE fname="Ember" AND lname="Bobember" AND birthdate="2018-03-16"),
+    (SELECT providerID FROM providers WHERE fname="Martha" AND lname="Mathers"),
+    (SELECT clinicID FROM clinics WHERE clinicName="Max Health Plaza"),
+    "Ember in today for wellness, vitals higher than normal,
+    adjusting medications, prescription sent to Walmart pharmacy"),
+("2020-02-18", "Wellness",
+    (SELECT diagnosisCode FROM diagnoses WHERE diagnosisName="Ingrown toenail"),
+    (SELECT procedureCode FROM procedures WHERE procedureName="General Checkup"),
+    (SELECT medicalRecordNumber FROM patients WHERE fname="Bob" AND lname="Burgers" AND birthdate="1956-12-10"),
+    (SELECT providerID FROM providers WHERE fname="John" AND lname="Smith"),
+    (SELECT clinicID FROM clinics WHERE clinicName="Max Health Plaza"),
+    "Mr. Burgers in for his wellness, reports pain in toe, upon examination found nail ingrown with mild
+    infection, antibiotics prescription sent to Walgreens, will come back next week for removal"),
+("2020-03-18", "Ingrown toenail removal",
+    (SELECT diagnosisCode FROM diagnoses WHERE diagnosisName="Ingrown toenail"),
+    (SELECT procedureCode FROM procedures
+        WHERE procedureName="Avulsion of nail plate, partial or complete, simple, single"),
+    (SELECT medicalRecordNumber FROM patients WHERE fname="Bob" AND lname="Burgers" AND birthdate="1956-12-10"),
+    (SELECT providerID FROM providers WHERE fname="John" AND lname="Smith"),
+    (SELECT clinicID FROM clinics WHERE clinicName="Max Health Plaza"),
+    "Inflammation down enough to remove ingrown toenail, no complications"),
+("2020-11-09", "Wellness",
+    (SELECT diagnosisCode FROM diagnoses WHERE diagnosisName="Torn meniscus"),
+    (SELECT procedureCode FROM procedures WHERE procedureName="General Checkup"),
+    (SELECT medicalRecordNumber FROM patients WHERE fname="Rick" AND lname="Sanchez" AND birthdate="1950-01-01"),
+    (SELECT providerID FROM providers WHERE fname="Martha" AND lname="Mathers"),
+    (SELECT clinicID FROM clinics WHERE clinicName="Max Health Plaza"),
+    "Rick came in for his wellness. Seems to have a significant limp. Patient
+    claimed it to be only giving him mild pain. Upon palpation and subsequent x-ray,
+    patient has slight tear in meniscus, will be referred to Max Orthopedics"),
+("2020-12-22", "Knee surgery for torn meniscus",
+    (SELECT diagnosisCode FROM diagnoses WHERE diagnosisName="Torn meniscus"),
+    (SELECT procedureCode FROM procedures WHERE procedureName="Arthroscopy, knee, surgical; with meniscectomy"),
+    (SELECT medicalRecordNumber FROM patients WHERE fname="Rick" AND lname="Sanchez" AND birthdate="1950-01-01"),
+    (SELECT providerID FROM providers WHERE fname="Gerald" AND lname="Bones"),
+    (SELECT clinicID FROM clinics WHERE clinicName="Max Orthopedics"),
+    "Meniscus cartilage shaved down where tear occurred, no complications.
+    Rick will be sent home with a walker and be referred to a week of physical therapy");
+
+INSERT INTO providersClinics (providerID, clinicID)
+VALUES ((SELECT providerID FROM providers WHERE fname="John" AND lname="Smith"),
+    (SELECT clinicID FROM clinics WHERE clinicName="Max Health Plaza")),
+((SELECT providerID FROM providers WHERE fname="Mary" AND lname="Jane"),
+    (SELECT clinicID FROM clinics WHERE clinicName="Max Health Plaza")),
+((SELECT providerID FROM providers WHERE fname="Martha" AND lname="Mathers"),
+    (SELECT clinicID FROM clinics WHERE clinicName="Max Health Plaza")),
+((SELECT providerID FROM providers WHERE fname="Gerald" AND lname="Bones"),
+    (SELECT clinicID FROM clinics WHERE clinicName="Max Orthopedics")),
+((SELECT providerID FROM providers WHERE fname="Jerry" AND lname="Knee"),
+    (SELECT clinicID FROM clinics WHERE clinicName="Max Orthopedics"));
+
+INSERT INTO patientsClinics (patientID, clinicID)
+VALUES
+((SELECT medicalRecordNumber FROM patients WHERE fname="Deborah" AND lname="Donahugh" AND birthdate="1972-06-12"),
+    (SELECT clinicID FROM clinics WHERE clinicName="Max Health Plaza")),
+((SELECT medicalRecordNumber FROM patients WHERE fname="Maddie" AND lname="Acosta" AND birthdate="1994-04-20"),
+    (SELECT clinicID FROM clinics WHERE clinicName="Max Health Plaza")),
+((SELECT medicalRecordNumber FROM patients WHERE fname="Ember" AND lname="Bobember" AND birthdate="2018-03-16"),
+    (SELECT clinicID FROM clinics WHERE clinicName="Max Health Plaza")),
+((SELECT medicalRecordNumber FROM patients WHERE fname="Bob" AND lname="Burgers" AND birthdate="1956-12-10"),
+    (SELECT clinicID FROM clinics WHERE clinicName="Max Health Plaza")),
+((SELECT medicalRecordNumber FROM patients WHERE fname="Rick" AND lname="Sanchez" AND birthdate="1950-01-01")
+    (SELECT clinicID FROM clinics WHERE clinicName="Max Health Plaza")),
+((SELECT medicalRecordNumber FROM patients WHERE fname="Rick" AND lname="Sanchez" AND birthdate="1950-01-01"),
+    (SELECT clinicID FROM clinics WHERE clinicName="Max Orthopedics"));
