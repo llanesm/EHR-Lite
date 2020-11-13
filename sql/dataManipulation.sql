18720f2bcbc8f05f35371b883ae04995937f94ba
Kevin Benoit <kevin.benoit8@gmail.com>
2020-11-13 11:14:50 -0700

    todo-list of manipulation queries needed

---
+++ sql/dataManipulation.sql
@@ -0,0 +1,30 @@
+-- Inserts --
+--TODO:
+--paitent
+--providers
+--clinic
+--visit
+
+
+
+-- Selects --
+--TODO:
+--patients, Select by medical record number
+--visit, Select by date
+--providers' paitents, Select by provider ID
+--clinics, Select by Clinic ID
+
+
+-- Deletes --
+--TODO:
+--patients, Select by medical record number
+--visit, Select by account number
+--providers, Select by Provider ID
+--clinics, Select by Clinic ID
+
+-- Updates --
+--TODO:
+--patients, Select by patient ID
+--visit, Select by account number
+--providers, Select by provider ID
+--clinics, Select by Clinic ID
