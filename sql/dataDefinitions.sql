b5e4b0bab1ee4d1c8a0f878476445cd9b98c169f
Kevin Benoit <kevin.benoit8@gmail.com>
2020-11-13 10:34:22 -0700

    Dropping in correct order

--- sql/dataDefinitions.sql
+++ sql/dataDefinitions.sql
@@ -1,4 +1,14 @@
 -- Tables initialization
+DROP TABLE IF EXISTS providersClinics;
+DROP TABLE IF EXISTS patientsClinics;
+
+DROP TABLE IF EXISTS visits;
+DROP TABLE IF EXISTS procedures;
+DROP TABLE IF EXISTS diagnoses;
+DROP TABLE IF EXISTS clinics;
+
+DROP TABLE IF EXISTS patients;
+DROP TABLE IF EXISTS providers;
 
 CREATE TABLE `clinics` (
     `clinicID` int(11) AUTO_INCREMENT NOT NULL,
@@ -76,30 +86,3 @@ CREATE TABLE `patientsClinics` (
     FOREIGN KEY (`clinicID`) REFERENCES clinics(`clinicID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- Insertions to populate each table.
-
-INSERT INTO clinics (clinicName, specialty, providerCapacity, examRooms, primaryCare)
-VALUES ("Health Plaza", "Family Medicine", 5, 5, TRUE);
-
-INSERT INTO providers (fname, lname, licenseType, licenseNumber, specialty, primaryCare)
-VALUES ("John", "Smith", "Medical Doctor", 1234, "Family Medicine", TRUE);
-
-INSERT INTO patients (fname, lname, birthdate, preferredPharmacy, primaryCarePhysician)
-VALUES ("Deborah", "Donahugh", "1972-06-12", "Walgreens",
-(SELECT providerID FROM providers WHERE fname="John" AND lname="Smith"));
-
-INSERT INTO procedures (procedureCode, procedureName)
-VALUES (99214, "General Checkup");
-
-INSERT INTO diagnoses (diagnosisCode, diagnosisName)
-VALUES ("I10", "Essential Hypertension");
-
-INSERT INTO visits (visitDate, chiefComplaint, diagnosisCode, procedureCode, patient, provider, clinicID, providerNotes)
-VALUES ("2020-03-20", "Wellness",
-    (SELECT diagnosisCode FROM diagnoses WHERE diagnosisName="Essential Hypertension"),
-    (SELECT procedureCode FROM procedures WHERE procedureName="General Checkup"),
-    (SELECT medicalRecordNumber FROM patients WHERE fname="Deborah" AND lname="Donahugh" AND birthdate="1972-06-12"),
-    (SELECT providerID FROM providers WHERE fname="John" AND lname="Smith"),
-    (SELECT clinicID FROM clinics WHERE clinicName="Health Plaza"),
-    "Deborah got her yearly wellness, hypertension continues to be stable on current medication"
-);
