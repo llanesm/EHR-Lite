03cb0f53e888436170b02bf21616553975779feb
llanesm <llanesm@oregonstate.edu>
2020-11-22 13:43:07 -0700

    CREATE and READ for admin page

--- ehr_lite_website/webapp.py
+++ ehr_lite_website/webapp.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from flask import request, redirect, url_for
+from flask import session
 from db_connector.db_connector import connect_to_database, execute_query
 
 import sys
@@ -53,9 +54,107 @@ def providers():
 
     return render_template('providers.html')
 
-@webapp.route('/admin')
+@webapp.route('/admin', methods=['GET', 'POST'])
 def admin():
     #setup for connecting to our database
     db_connection = connect_to_database()
 
+    try:
+        session['patientData']
+        session['providerData']
+    except KeyError as error:
+        session['patientData'] = 0
+        session['providerData'] = 0
+        print("caught keyerror", error)
+
+    if session['patientData']:
+        patientData = session['patientData']
+    else:
+        session['patientData'] = {}
+
+    if session['providerData']:
+        providerData = session['providerData']
+    else:
+        providerData = {}
+
+    if request.method == 'POST':
+
+        # Add new provider
+        if 'adminNewProvider' in request.form:
+            print('ADDING NEW PROVIDER')
+
+            fname = request.form['fname']
+            lname = request.form['lname']
+            licenseType = request.form['licenseType']
+            licenseNumber = request.form['licenseNumber']
+            specialty = request.form['specialty']
+            primaryCare = request.form['primaryCare']
+
+            query = """
+                    INSERT INTO providers (fname, lname, licenseType, licenseNumber, specialty, primaryCare)
+                    VALUES ('{}', '{}', '{}', '{}', '{}', '{}');
+                    """.format(fname, lname, licenseType, licenseNumber, specialty, primaryCare)
+            execute_query(db_connection, query)
+
+        # Add new clinic
+        elif 'adminNewClinic' in request.form:
+            print('ADDING NEW CLINIC')
+
+            specialty = request.form['specialty']
+            providerCapacity = request.form['providerCapacity']
+            examRooms = request.form['examRooms']
+            primaryCare = request.form['primaryCare']
+
+            query = """
+                    INSERT INTO clinics (clinicName, specialty, providerCapacity, examRooms, primaryCare)
+                    VALUES ('{}', '{}', '{}', '{}');
+                    """.format(specialty, providerCapacity, examRooms, primaryCare)
+            execute_query(db_connection, query)
+
+        # Display table of patients that are patients of a given clinic
+        elif 'adminsViewPatients' in request.form:
+            print('LOOKING UP CLINIC')
+
+            patientsClinicID = request.form['clinicID']
+
+            query = """SELECT patients.medicalRecordNumber, patients.fname, patients.lname, patients.birthdate
+                        FROM patients
+                        JOIN visits ON visits.patient = patients.medicalRecordNumber
+                        JOIN clinics ON clinics.clinicID = visits.clinic
+                        WHERE clinics.clinicID = '{}';""".format(patientsClinicID)
+            result = execute_query(db_connection, query)
+
+            row_headers = [x[0] for x in result.description]
+            row_variables = result.fetchall()
+            json_data = []
+            for row_string in row_variables:
+                json_data.append(dict(zip(row_headers, row_string)))
+            patientData = json_data
+            session['patientData'] = patientData
+
+        # Display table of providers that practice at a given clinic
+        elif 'adminsViewProviders' in request.form:
+            print('LOOKING UP CLINIC')
+
+            providersClinicID = request.form['clinicID']
+
+            query = """SELECT providers.providerID, providers.fname, providers.lname, providers.licenseType,
+                        providers.licenseNumber, providers.specialty, providers.primaryCare
+                        FROM providers
+                        JOIN providersClinics ON providersClinics.providerID = providers.providerID
+                        WHERE providersClinics.clinicID = '{}';""".format(providersClinicID)
+            result = execute_query(db_connection, query)
+
+            row_headers = [x[0] for x in result.description]
+            row_variables = result.fetchall()
+            json_data = []
+            for row_string in row_variables:
+                json_data.append(dict(zip(row_headers, row_string)))
+            providerData = json_data
+            session['providerData'] = providerData
+
+        return render_template('admin.html', patientData=patientData, providerData=providerData)
+
     return render_template('admin.html')
+
+webapp.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
